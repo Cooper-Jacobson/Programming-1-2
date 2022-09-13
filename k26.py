9172714378695d30e4e0d8518e0b322b5a7e2253
Cooper Jacobson <113038437+Cooper-Jacobson@users.noreply.github.com>
2022-09-13 14:12:07 +0000

    Update

---
+++ k26.py
@@ -0,0 +1,32 @@
+from stanfordkarel import *
+
+
+def main():
+    """ Karel code goes here! """
+    pick_beeper()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    put_beeper()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    move()
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
