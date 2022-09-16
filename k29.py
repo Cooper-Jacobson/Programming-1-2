7ce4f0e76088b28e2c3b79ec2eb142bea33ddab0
Cooper Jacobson <113038437+Cooper-Jacobson@users.noreply.github.com>
2022-09-16 14:22:34 +0000

    Update

--- k29.py
+++ k29.py
@@ -3,11 +3,36 @@ from stanfordkarel import *
 
 def main():
     """ Karel code goes here! """
+    move()
+    move()
+    move()
+    move()
     turn_left()
     move()
-    
-    pass
-
-
-if __name__ == "__main__":
-    run_karel_program()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+  pass
+  if __name__ == "__main__":
+  run_karel_program()
