8d0a6be3be41a9f7b9454009c32125598df5c15b
Cooper Jacobson <113038437+Cooper-Jacobson@users.noreply.github.com>
2022-09-19 14:28:02 +0000

    Update

--- k32.py
+++ k32.py
@@ -1,9 +1,76 @@
 from stanfordkarel import *
 
 
+class ktools:
+  def m(self):
+    """Shorthand for move"""
+    move()
+
+  def tl(self):
+    """Turn Left"""
+    turn_left()
+
+  def tr(self):
+      """Turn Right"""
+      self.tl()
+      self.tl()
+      self.tl()
+
+  def ta(self)
+   """Turn Around"""
+        self.tl()
+        self.tl()
+
+  def pick(self):
+    """Pick Beeper"""
+    pick_beeper()
+
+  def put(self):
+    """Put Beeper"""
+    put_beeper()
+
 def main():
     """ Karel code goes here! """
-    
+    self.m()
+    self.m()
+    self.put()
+    self.tl()
+    self.m()
+    self.tl()
+    self.m()
+    self.put()
+    self.tr()
+    self.m()
+    self.m()
+    self.put()
+    self.m()
+    self.tl()
+    self.m()
+    self.put()
+    self.tl()
+    self.m()
+    self.m()
+    self.put()
+    self.m()
+    self.m()
+    self.put()
+    self.m()
+    self.tl()
+    self.m()
+    self.put()
+    self.tr()
+    self.m()
+    self.m()
+    self.put()
+    self.m()
+    self.m()
+    self.put()
+    self.m()
+    self.m()
+    self.put()
+    self.m()
+    self.m()
+    self.put()
     pass
 
 
--- k34.py
+++ k34.py
@@ -1,9 +1,95 @@
 from stanfordkarel import *
 
 
+class ktools:
+  def m(self):
+    """Shorthand for move"""
+    move()
+
+  def tl(self):
+    """Turn Left"""
+    turn_left()
+
+  def tr(self):
+      """Turn Right"""
+      self.tl()
+      self.tl()
+      self.tl()
+
+  def ta(self)
+   """Turn Around"""
+        self.tl()
+        self.tl()
+
+  def pick(self):
+    """Pick Beeper"""
+    pick_beeper()
+
+  def put(self):
+    """Put Beeper"""
+    put_beeper()
+
 def main():
     """ Karel code goes here! """
-    
+    self.m()
+    self.put()
+    self.tl()
+    self.m()
+    self.tl()
+    self.m()
+    self.put()
+    self.tr()
+    self.m()
+    self.m()
+    self.put()
+    self.m()
+    self.tl()
+    self.m()
+    self.put()
+    self.m()
+    self.m()
+    self.put()
+    self.m()
+    self.m()
+    self.put()
+    self.m()
+    self.tl()
+    self.m() 
+    self.put()
+    self.tr()
+    self.m()
+    self.m()
+    self.put()
+    self.m()
+    self.m()
+    self.put()
+    self.m()
+    self.m()
+    self.put()
+    self.tl()
+    self.m()
+    self.tl()
+    self.m()
+    self.put()
+    self.m()
+    self.m()
+    self.put()
+    self.m()
+    self.m()
+    self.put()
+    self.tl()
+    self.m()
+    self.tr()
+    self.m()
+    self.put()
+    self.m()
+    self.m()
+    self.put()
+    self.tl()
+    self.m()
+    self.tl()
+    self.m()
+    self.put()
     pass
 
 
