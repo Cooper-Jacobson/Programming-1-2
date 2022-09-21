ab3e3e25bb116f2ee299c1b9ec4ac2448b83d88a
Cooper Jacobson <113038437+Cooper-Jacobson@users.noreply.github.com>
2022-09-21 14:31:44 +0000

    Update

---
+++ prog54c.py
@@ -0,0 +1,9 @@
+radius = int(input("Please enter the radius: "))
+area = int(input("Please enter the area: "))
+circumference = int(input("Plese enter the circumference :"))
+
+print ("The radius is", radius)
+print ("The area is", area)
+print ("The circumference is", )
+
+radius = 
