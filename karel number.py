e9eefe8e7dcba32a86e1a5cb372a8bb6917d9aee
Cooper Jacobson <113038437+Cooper-Jacobson@users.noreply.github.com>
2022-09-22 14:37:51 +0000

    Update

---
+++ karel number.py
@@ -0,0 +1,81 @@
+from stanfordkarel import *
+
+
+class ktools:
+  def m(self):
+    """Shorthand for move"""
+    move()
+
+  def tl(self):
+    """Turn Left"""
+    turn_left()
+
+  def tr(self):
+      """Turn Right"""
+      self.tl()
+      self.tl()
+      self.tl()
+
+  def ta(self)
+   """Turn Around"""
+        self.tl()
+        self.tl()
+
+  def pick(self):
+    """Pick Beeper"""
+    pick_beeper()
+
+  def put(self):
+    """Put Beeper"""
+    put_beeper()
+
+  def put2(self):
+    """Put 2 beepers in a line"""
+    self.put()
+    self.m
+    self.put()
+
+  def put5(self):
+    """Put 5 beepers in a line"""
+    self.put2()
+    self.m()
+    self.put2()
+    self.m()
+    self.put()
+
+    def mm(self, num):
+  """Move Multiple"""
+  for number in range(num):
+    self.m()
+
+def pickm(self, num):
+  """Pick Multiple"""
+  for i in range(num-1):
+    self.pick()
+    self.m()
+  self.pick()
+
+def putm(self, num):
+  """Put Multiple"""
+  for _ in range (0, num-1):
+    self.put()
+    self.m()
+  self.put()
+
+def main():
+  """ Karel code goes here! """
+  kt = ktools()
+  kt.tl()
+  kt.mm(5)
+  kt.ta()
+  kt.putm(5)
+  kt.tl()
+  kt.mm(2)
+
+
+  pass  
+
+
+if __name__ == "__main__":
+    run_karel_program()
+    
