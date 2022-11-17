cda522efc052d233b56a3a64e11a69283a953344
Cooper Jacobson <113038437+Cooper-Jacobson@users.noreply.github.com>
2022-11-17 15:04:49 +0000

    Update

--- LP4-2.py
+++ LP4-2.py
@@ -1,5 +1,19 @@
 def main():
-  
+  weight = int(input("Enter package weight in kilograms: "))
+  length = int(input("Enter package length in centimeters: "))
+  width = int(input("Enter package width in centimeters: "))
+  height = int(input("Enter package height in centimeters: "))
+
+  if weight > 27:
+    print("Too heavy")
+  elif length > 0.1:
+    print("Too large")
+  elif width > 0.1:
+    print("Too large")
+  elif height > 0.1:
+    print("Too large")
+  elif weight > 27 and length > 0.1:
+    print("Too heavy and too large")
   pass
 
 
