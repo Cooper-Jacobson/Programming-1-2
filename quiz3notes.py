be650e5c928c5001d73acc854ecd1786d1a72622
Cooper Jacobson <113038437+Cooper-Jacobson@users.noreply.github.com>
2022-11-28 15:08:12 +0000

    Update

--- quiz3notes.py
+++ quiz3notes.py
@@ -2,8 +2,10 @@ text = input("Enter text: ")
 for index in range(len(text)):
   print(text[index], end=" ")
 print()
-
+numwords = 0
 for letter in text:
+    if letter == " ":
+    numwords = numwords + 1
   print(letter)
 def main():
   text = "cool beans"
